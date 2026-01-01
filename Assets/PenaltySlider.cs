a09cb39f2756e926c8feb79ff14c97dad223c964
crabnebuelar <thecrabnebuelar@gmail.com>
2025-12-31 20:23:44 -0500

    Third Location

--- Assets/PenaltySlider.cs
+++ Assets/PenaltySlider.cs
@@ -9,10 +9,13 @@ public class PenaltySlider : MonoBehaviour
     [SerializeField] private Slider slider;
     [SerializeField] private Text text;
     [SerializeField] private int lambda;
+    [SerializeField] private int defaultValue;
 
     private void OnEnable()
     {
+        slider.value = defaultValue;
         slider.onValueChanged.AddListener(OnSliderChanged);
+        text.text = defaultValue.ToString();
     }
 
     void OnSliderChanged(float value)
@@ -24,5 +27,11 @@ public class PenaltySlider : MonoBehaviour
     private void OnDisable()
     {
         slider.onValueChanged.RemoveListener(OnSliderChanged);
+    }
+
+    public void Default()
+    {
+        slider.value = defaultValue;
+        text.text = defaultValue.ToString();
     }
 }
