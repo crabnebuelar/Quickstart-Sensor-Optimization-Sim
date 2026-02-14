ade793eea4fe00b20c44f5684a0e1930c31b9897
Adriel Motter <59982949+crabnebuelar@users.noreply.github.com>
2026-02-14 12:15:45 -0500

    Update sensor_optimizer.py

--- Assets/StreamingAssets/sensor_optimizer.py
+++ Assets/StreamingAssets/sensor_optimizer.py
@@ -9,7 +9,7 @@ from qiskit_optimization import QuadraticProgram
 from qiskit_optimization.algorithms import MinimumEigenOptimizer
 from qiskit_algorithms.optimizers import COBYLA
 
-
+# Debugging
 '''try:
     print("PYTHON STARTED")
     sys.stdout.flush()
@@ -25,6 +25,7 @@ except Exception as e:
         f.write(str(e))
     raise'''
 
+# Initialize paths and coverage matrix
 test_path = sys.argv[2] if len(sys.argv) > 2 else "test_output.txt"
 
 with open(test_path, "w") as f:
@@ -33,43 +34,40 @@ with open(test_path, "w") as f:
 print("Wrote test file to:", test_path)
 sys.stdout.flush()
 
-
-
-C = [[int(x) for x in row.split(",")]
+coverage_matrix = [[int(x) for x in row.split(",")]
     for row in sys.argv[1].split(";")]
 
 output_path = sys.argv[2]
 
-
-
-'''C = [
+'''coverage_matrix = [
     [1,1,0,0,0,0],  # S1
     [0,1,1,0,0,0],  # S2
     [0,0,1,1,1,0],  # S3
     [0,0,0,0,1,1],  # S4
 ]'''
 
+# Initialize penalties
 lambda1 = float(sys.argv[3])
 lambda2 = float(sys.argv[4])
 lambda3 = float(sys.argv[5])
 
-num_sensors = len(C)
-num_positions = len(C[0])
+num_sensors = len(coverage_matrix)
+num_positions = len(coverage_matrix[0])
 
 # Define small QUBO
 qp = QuadraticProgram()
 for i in range(num_sensors):
     qp.binary_var(f"x{i+1}")
 
-# qp.minimize(linear={f"x{i+1}": lambda1 for i in range(4)})
 
+# Initialize linear, quadratic, and constant objectives
 linear_obj = {f"x{i+1}": lambda1 for i in range(num_sensors)}  # sensor cost
 quadratic_obj = {}
 constant_obj = 0
 
 # Add coverage penalties
 for j in range(num_positions):
-    sensors_covering_j = [i for i in range(num_sensors) if C[i][j]==1]
+    sensors_covering_j = [i for i in range(num_sensors) if coverage_matrix[i][j]==1]
     # Linear
     for i in sensors_covering_j:
         linear_obj[f"x{i+1}"] = linear_obj.get(f"x{i+1}",0) - 2*lambda2
@@ -82,21 +80,18 @@ for j in range(num_positions):
     # Constant
     constant_obj += lambda2
 
-# Call minimize once
 qp.minimize(linear=linear_obj, quadratic=quadratic_obj, constant=constant_obj)
 
 
-# 2. Choose QAOA optimizer
-# service = QiskitRuntimeService(channel="ibm_cloud")
-
-# 2. Initialize sampler (no backend argument)
+# Initialize sampler
 sampler = StatevectorSampler()
 qaoa = QAOA(sampler=sampler, optimizer=COBYLA(maxiter=50), reps=1)
 meo = MinimumEigenOptimizer(qaoa)
 
-# 3. Solve
+# Solve
 result = meo.solve(qp)
 
+# Put solution in json and output
 solution = {
     "selected_sensors": [
         {"key": f"S{i}", "value": float(result[f"x{i+1}"])}
